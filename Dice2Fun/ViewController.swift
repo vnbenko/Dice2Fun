9752eb537f533ee9fcb972a3c486bbb8635f7fd5
Vladimir Benko <vnbenko@gmail.com>
2022-12-15 00:00:16 +0300

    TASK-1. Create simple dice app

--- Dice2Fun/ViewController.swift
+++ Dice2Fun/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  Dice2Fun
-//
-//  Created by Vladimir Benko on 14.12.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftDiceImageView: UIImageView?
+    @IBOutlet weak var rightDiceImageView: UIImageView?
+    
+    let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        animateDices(start: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.animateDices(start: false)
+            self.leftDiceImageView?.image = self.dices[Int.customRandom(max: 5)]
+            self.rightDiceImageView?.image = self.dices[Int.random(in: 0...5)]
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func animateDices(start: Bool) {
+        if start == true {
+            leftDiceImageView?.animationImages = dices
+            rightDiceImageView?.animationImages = dices
+            leftDiceImageView?.animationDuration = 0.3
+            rightDiceImageView?.animationDuration = 0.3
+            leftDiceImageView?.startAnimating()
+            rightDiceImageView?.startAnimating()
+        } else {
+            leftDiceImageView?.stopAnimating()
+            rightDiceImageView?.stopAnimating()
+        }
+    }
+    
 }
 
+extension Int {
+    
+    static func customRandom(max number: Int = 10) -> Int {
+        var collection: Set<Int> = []
+        var randomValue = 0
+        
+        for value in 0...number {
+            collection.insert(value)
+        }
+        for value in collection {
+            randomValue = value
+        }
+        return randomValue
+    }
+}
